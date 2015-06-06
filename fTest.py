befc6bb95f2f0948b148e5af53420ae5519471c1
Howard Burrows <ghburrows@comcast.net>
2015-06-06 12:20:56 -0400

    Func test now checks that we can input to-do item

--- fTest.py
+++ fTest.py
@@ -1,5 +1,6 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisTest(unittest.TestCase):
@@ -17,6 +18,28 @@ class NewVisTest(unittest.TestCase):
 
         #Note ToDo in title
         self.assertIn('ToDo', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('ToDo', header_text)
+
+        # Add to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
+
+        # Enter some text into text box
+        inputbox.send_keys('Buy feathers')
+
+        # Then hit enter, pg updates, displays list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy feathers' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
         #will add story here...
