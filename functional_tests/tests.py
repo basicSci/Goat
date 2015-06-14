3afbf9574e6e73e448ec4084da6dcf6f698b80d9
Howard Burrows <ghburrows@comcast.net>
2015-06-14 17:14:23 -0400

    Added tests for second user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -37,6 +37,8 @@ class NewVisTest(LiveServerTestCase):
 
         # Then hit enter, pg updates, displays list
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy feathers')
 
         #Add another item
@@ -47,6 +49,35 @@ class NewVisTest(LiveServerTestCase):
         # Page updates and shows both items
         self.check_for_row_in_list_table('1: Buy feathers')
         self.check_for_row_in_list_table('2: Use peacocks')
+
+        # Now a new user
+
+        ## We use a new browser session to check info flow
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # New user Francis visits; check no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy feathers', page_text)
+        self.assertNotIn('Use peacocks', page_text)
+
+        # Francis starts new list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis gets his own unique URL
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
 
         self.fail('Finish the test!')
 
