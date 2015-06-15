05e76f1dd65d21bb4d39b0ddcd35785d1e1b0505
Howard Burrows <ghburrows@comcast.net>
2015-06-14 21:05:36 -0400

    Adjust model so items are in different lists

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import redirect,render
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only/')
     return render(request, 'home.html')
 
 def view_list(request):
@@ -12,5 +9,6 @@ def view_list(request):
     return render(request, 'list.html', {'items':items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only/')
